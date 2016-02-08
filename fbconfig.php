881b604f82e67078aad3880ec25cbb9e1a3daab6
Tanmay <tkpandey@hotmail.com>
2016-02-08 12:38:17 +0530

    new page with bootstrap

--- fbconfig.php
+++ fbconfig.php
@@ -2,9 +2,9 @@
 session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 // added in v4.0.0
-require_once 'autoload.php';
-use Facebook\FacebookSession;
-use Facebook\FacebookRedirectLoginHelper;
+//require_once 'autoload.php';
+/*use Facebook\FacebookSession;
+use Facebook\Helpers\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\FacebookResponse;
 use Facebook\FacebookSDKException;
@@ -14,16 +14,27 @@ use Facebook\GraphObject;
 use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
+*/
 // init app with app id and secret
-FacebookSession::setDefaultApplication( '1687233988161207','15c0aa4edbd65f6a01ee26a9c07575c6');
+
+$fb = new Facebook\Facebook([
+  'app_id' => '1687233988161207',
+  'app_secret' => '15c0aa4edbd65f6a01ee26a9c07575c6',
+  'default_graph_version' => 'v2.5',
+]);
 // login helper with redirect_uri
-    $helper = new FacebookRedirectLoginHelper('http://scrapper.odap.cf/fbconfig.php' );
+$helper = $fb->getRedirectLoginHelper();
+    //$helper = new FacebookRedirectLoginHelper('http://scrapper.odap.cf/fbconfig.php' );
 try {
-  $session = $helper->getSessionFromRedirect();
-} catch( FacebookRequestException $ex ) {
-  // When Facebook returns an error
-} catch( Exception $ex ) {
+  $accessToken = $helper->getAccessToken();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
   // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
 }
 // see if we have a session
 if ( isset( $session ) ) {
