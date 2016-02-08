a520beea2e4f2ebe7a42de0297f00b8a06891e6a
Tanmay <tkpandey@hotmail.com>
2016-02-08 20:00:33 +0530

    new page with bootstrap

--- fbconfig.php
+++ fbconfig.php
@@ -68,7 +68,7 @@ if (! $accessToken->isLongLived()) {
 }
 
 $_SESSION['fb_access_token'] = (string) $accessToken;
-
+var_dump($_SESSION);
 // User is logged in with a long-lived access token.
 // You can redirect them to a members-only page.
 //header('Location: https://example.com/members.php');
