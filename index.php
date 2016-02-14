fa4dc6fb5e446909b7eb3e67e10fb55bf1bc5b7c
Tanmay <tkpandey@hotmail.com>
2016-02-14 22:38:58 +0530

    debugging

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ function logout() {
               </tr>
               <tr>
                 <td>Education:</td>
-                <td><?php str_replace("Array","",print_r($_SESSION['EDUCATION']));//foreach($_SESSION['EDUCATION'] as $key=>$value){ foreach($value as $key1=>$value){print_r($value)."</br>";} } ?></td>
+                <td><?php print(str_replace("Array","",var_dump($_SESSION['EDUCATION'])));  //foreach($_SESSION['EDUCATION'] as $key=>$value){ foreach($value as $key1=>$value){print_r($value)."</br>";} } ?></td>
               </tr>
               <tr>
                 <td>Political:</td>
